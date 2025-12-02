fe6ee54b6939c4bb18d6e63604b40059fd4a9003
ByBresh <contact@bybresh.com>
2025-12-02 18:12:57 +0100

    Add PostgreSQL hosting support to the application
    Added `Aspire.Hosting.PostgreSQL` (v9.5.2) to `Directory.Packages.props`
    and `Tiendasp.Host.csproj` to enable PostgreSQL hosting functionality.
    
    Configured PostgreSQL in `AppHost.cs` using `AddPostgres` with:
    - Persistent container lifetime
    - Data volume `tiendasp-postgres`
    - PgAdmin setup with host port 5050
    
    These changes enable the application to integrate with a PostgreSQL
    database and provide database management via PgAdmin.

--- Tiendasp.Host/AppHost.cs
+++ Tiendasp.Host/AppHost.cs
@@ -1,3 +1,9 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var postgres = builder
+    .AddPostgres("postgres")
+    .WithLifetime(ContainerLifetime.Persistent)
+    .WithDataVolume("tiendasp-postgres")
+    .WithPgAdmin(pgAdmin => pgAdmin.WithHostPort(5050));
+
 builder.Build().Run();
