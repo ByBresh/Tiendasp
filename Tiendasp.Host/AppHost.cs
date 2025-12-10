96b4f36fc2345d3108276db3a6050b50deb5e255
ByBresh <contact@bybresh.com>
2025-12-10 18:04:08 +0100

    Add RabbitMQ service and references to app host
    Configured RabbitMQ with persistent storage and management plugin in AppHost.cs. Updated Tiendasp_API_Identity to wait for both PostgreSQL and RabbitMQ. Added Aspire.Hosting.RabbitMQ package and project reference to Tiendasp.API.Identity in Tiendasp.Host.csproj.

--- Tiendasp.Host/AppHost.cs
+++ Tiendasp.Host/AppHost.cs
@@ -1,9 +1,23 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var rabbit = builder
+    .AddRabbitMQ("rabbitmq")
+    .WithLifetime(ContainerLifetime.Persistent)
+    .WithDataVolume("tiendasp-rabbitmq-data")
+    .WithManagementPlugin();
+
 var postgres = builder
     .AddPostgres("postgres")
     .WithLifetime(ContainerLifetime.Persistent)
     .WithDataVolume("tiendasp-postgres")
     .WithPgAdmin(pgAdmin => pgAdmin.WithHostPort(5050));
+
+var db = postgres.AddDatabase("identity");
+
+builder.AddProject<Projects.Tiendasp_API_Identity>("tiendasp-api-identity")
+    .WaitFor(db)
+    .WaitFor(rabbit)
+    .WithReference(db)
+    .WithReference(rabbit);
 
 builder.Build().Run();
